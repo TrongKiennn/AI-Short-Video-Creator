113b077006613c441c7b02593cb268b69087c6c0
Manh Trong Kien <manhtrongkien1901@gmail.com>
2025-05-16 19:34:14 +0700

    feat(ui, database): set up database and add sidebar component
    - configure initial database setup
    - add sidebar UI with navigation links

--- app/provider.js
+++ app/provider.js
@@ -4,30 +4,41 @@ import {ThemeProvider as NextThemesProvider} from "next-themes"
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from '@/configs/firebaseConfig'
 import { AuthContext } from './_context/AuthContext'
-
+import { ConvexProvider, ConvexReactClient, useMutation } from "convex/react";
+import {api} from '@/convex/_generated/api'
 
 function Provider({children}) {
   const [user,setUser]=useState();
+  const CreateUser=useMutation(api.users.CreateNewUser)  
   useEffect(()=>{
-    const unsubcribe=onAuthStateChanged(auth,(user)=>{
+    const unsubcribe=onAuthStateChanged(auth,async (user)=>{
       console.log(user);
       setUser(user);
+
+      const result =await CreateUser({
+          name:user?.displayName,
+          email:user?.email,
+          pictureURL:user?.photoURL,
+      });
+      console.log(result)
     })
     return ()=>unsubcribe();
   },[])
 
   return (
     <div>
-        <AuthContext.Provider value={{user}}>
-          <NextThemesProvider
-              attribute="class"
-              defaultTheme="dark"
-              enableSystem
-              disableTransitionOnChange
-          >
-              {children}
-          </NextThemesProvider>
-        </AuthContext.Provider>
+       
+          <AuthContext.Provider value={{user}}>
+            <NextThemesProvider
+                attribute="class"
+                defaultTheme="dark"
+                enableSystem
+                disableTransitionOnChange
+            >
+                {children}
+            </NextThemesProvider>
+          </AuthContext.Provider>
+
     </div>
   )
 }
